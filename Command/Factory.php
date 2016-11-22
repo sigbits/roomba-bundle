b39ea5a3560bb2677dcf9923bf4a25d34a8a3bf6
Maarten van Leeuwen <maarten@sigbits.nl>
2016-11-22 20:31:00 +0000

    Command Factory for SCI Commands (needed for mapping names to FQCN)

---
+++ Command/Factory.php
@@ -0,0 +1,87 @@
+<?php
+namespace Sigbits\RoombaBundle\Command;
+use Sigbits\RoombaLib\SCI\Command as SCICommand;
+/**
+ * Created by PhpStorm.
+ * User: Maarten van Leeuwen <maarten@sigbits.nl>
+ * Date: 22/11/2016
+ */
+class Factory
+{
+    /**
+     * associative array. key = name, value is FQCN of the corresponding SCI command
+     *
+     * @var array
+     */
+    private $SCICommands;
+
+    /**
+     * Factory constructor.
+     * @param array $commands
+     */
+    public function __construct(array $commands)
+    {
+        foreach ($commands as $name => $fqcn) {
+            $this->addSCICommand($name, $fqcn);
+        }
+    }
+
+    /**
+     * @param $name
+     * @param $fqcn
+     */
+    private function addSCICommand($name, $fqcn)
+    {
+        $this->assertIsSCICommandClass($fqcn);
+
+        $SCICommands[$name] = $fqcn;
+    }
+
+    /**
+     * @param $fqcn
+     * @throws \InvalidArgumentException
+     */
+    private function assertIsSCICommandClass($fqcn)
+    {
+        if (!is_a($fqcn, '\Sigbits\RoombaLib\SCI\Command')) {
+            throw new \InvalidArgumentException(sprintf('FQCN %s is not a Sigbits\RoombaLib\SCI\Command', $fqcn));
+        }
+    }
+
+    /**
+     * @param string $name
+     */
+    public function hasCommand($name)
+    {
+        return (array_key_exists($name, $this->SCICommands));
+    }
+
+    /**
+     * @param string $cmdClass
+     * @return bool
+     */
+    private function isDataCommandClass($cmdClass)
+    {
+        return (is_a($cmdClass, '\Sigbits\RoombaLib\SCI\Command\AbstractDataCommand'));
+    }
+
+    /**
+     * @param string $name
+     * @param mixed | null $data
+     * @return Command
+     */
+    public function create($name, $data = null)
+    {
+        if (!$this->hasCommand($name)) {
+            throw new \InvalidArgumentException(sprintf('Command Factory has no SCI Command names %s', $name));
+        }
+
+        $cmdClass = $this->SCICommands[$name];
+
+        if ($this->isDataCommandClass($cmdClass)) {
+            return new $cmdClass($data);
+        } else {
+            return new $cmdClass();
+        }
+    }
+}
