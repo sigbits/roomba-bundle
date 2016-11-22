7559b842f5dff202b9fa8007af9e2a1be6f03b29
Maarten van Leeuwen <maarten@sigbits.nl>
2016-11-22 19:52:25 +0000

    general bugfixes

--- Console/RoombaExecuteCommand.php
+++ Console/RoombaExecuteCommand.php
@@ -1,5 +1,6 @@
 <?php
-namespace Sigbits\RoombaLib\Console;
+namespace Sigbits\RoombaBundle\Console;
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
--- Console/RoombaListCommand.php
+++ Console/RoombaListCommand.php
@@ -1,5 +1,5 @@
 <?php
-namespace Sigbits\RoombaLib\Console;
+namespace Sigbits\RoombaBundle\Console;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
